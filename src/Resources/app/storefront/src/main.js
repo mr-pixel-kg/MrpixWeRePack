b64f2ac012eabeb476bd40a282f72f901d6f661d
Manuel Kienlein <manuel.kienlein@gmx.de>
2022-10-14 14:08:52 +0200

    removed js checkbox from cart

--- src/Resources/app/storefront/src/main.js
+++
@@ -1,4 +0,0 @@
-import MrpixWeRepackPlugin from './mrpix-we-repack/mrpix-we-repack.plugin';
-
-const PluginManager = window.PluginManager;
-PluginManager.register('MrpixWeRepackPlugin', MrpixWeRepackPlugin, '.js-offcanvas-cart-add-we-repack');
