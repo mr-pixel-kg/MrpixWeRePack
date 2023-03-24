cee71b5da931b8cf5554c2fdbee0d8f8c1946782
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-03-24 12:18:56 +0100

    Add WeRepack details to order in administration

---
+++ src/Resources/app/administartion/src/module/sw-order/component/mrpixwerepack-order/index.js
@@ -0,0 +1,45 @@
+import template from './mrpixwerepack-order.html.twig';
+import  './mrpixwerepack-order.scss';
+
+const {Component, Context} = Shopware;
+const {Criteria} = Shopware.Data;
+
+Component.register('sw-customer-werepack', {
+    template,
+
+    data() {
+        return {
+            weRepackOrder: null
+        }
+    },
+
+    inject: ['repositoryFactory'],
+
+    props: {
+        order: {
+            type: Object,
+            required: true
+        },
+        isLoading: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+
+    computed: {
+        repository() {
+            return this.repositoryFactory.create('mp_repack_order');
+        }
+    },
+
+    created() {
+        const criteria = (new Criteria())
+            .addFilter(Criteria.equals("orderId", this.order.id));
+
+        this.repository.search(criteria, Context.api)
+            .then(result => {
+                this.weRepackOrder = result.first()
+            });
+    }
+});
---
+++ src/Resources/app/administartion/src/module/sw-order/index.js
@@ -0,0 +1,2 @@
+import './component/mrpixwerepack-order';
+import './view/sw-order-detail-base';
