81124163b51f24658e2ff24311608a01ac328d34
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-03-27 10:11:14 +0200

    Added ConfigService

---
+++ src/Service/ConfigService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mrpix\WeRepack\Service;
+
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+
+class ConfigService
+{
+    public const SYSTEM_CONFIG_DOMAIN = 'MrpixWeRepack.config.';
+    private $systemConfigService;
+
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    public function get(string $key, ?string $salesChannelId = null)
+    {
+        return $this->getSystemConfigService()->get(self::SYSTEM_CONFIG_DOMAIN.$key, $salesChannelId);
+    }
+
+    public function set(string $key, string $value, ?string $salesChannelId = null): void
+    {
+        $this->getSystemConfigService()->set(self::SYSTEM_CONFIG_DOMAIN.$key, $value, $salesChannelId);
+    }
+
+    public function delete(string $key, ?string $salesChannelId = null): void
+    {
+        $this->getSystemConfigService()->delete(self::SYSTEM_CONFIG_DOMAIN.$key, $salesChannelId);
+    }
+
+    public function getSystemConfigService(): SystemConfigService
+    {
+        return $this->systemConfigService;
+    }
+}
