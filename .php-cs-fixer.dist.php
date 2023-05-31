e7fc0da5d8ddc8d4b1eb09574eb74a1bbe7ad767
Manuel Kienlein <manuel.kienlein@mr-pixel.de>
2023-05-31 23:04:21 +0200

    ci: improved ci/cd pipeline

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('Resources');
+
+return (new PhpCsFixer\Config())
+    ->setUsingCache(false)
+    ->setRules([
+        '@PSR2' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => true,
+        'align_multiline_comment' => true,
+        'array_indentation' => true,
+        'no_superfluous_elseif' => true,
+        'not_operator_with_successor_space' => false,
+        'phpdoc_add_missing_param_annotation' => true,
+        'phpdoc_order' => true,
+        'phpdoc_trim_consecutive_blank_line_separation' => true,
+        'phpdoc_types_order' => true,
+        'yoda_style' => true,
+    ])
+    ->setFinder($finder);
